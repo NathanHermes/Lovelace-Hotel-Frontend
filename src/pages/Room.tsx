e71c1942432f4eb1dd677a25462f6430caa54879
NathanHermes <hermesnathan8@gmail.com>
2023-05-16 20:35:41 -0300

    :sparkles: Feat: init implementation of pages

---
+++ src/pages/Room.tsx
@@ -0,0 +1,13 @@
+export function Room() {
+  return (
+    <div className="flex">
+      <h1 className="font-black">Hóspedes</h1>
+      <p>
+        Gerencie todos os seus hóspedes existentes ou adicione um novo hóspede.
+      </p>
+
+      <input type="text" />
+      <button>Add Hóspede</button>
+    </div>
+  );
+}
