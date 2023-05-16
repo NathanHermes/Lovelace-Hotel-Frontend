114ddfe61a0e93c5a81493f6bc3c8bf7492f115b
NathanHermes <hermesnathan8@gmail.com>
2023-05-16 20:47:19 -0300

    :lipstick: Feat: import style file

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { routes } from "./routes";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
