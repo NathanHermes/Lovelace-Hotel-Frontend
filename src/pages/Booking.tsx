661f6316a7f84e01aca465bd7af30df0f84aea0f
NathanHermes <hermesnathan8@gmail.com>
2023-05-17 20:21:39 -0300

    :sparkles: Feat: change local components to global components

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,7 +1,17 @@
+import { Header } from "../components/Header";
+import { Navbar } from "../components/Navbar";
+import { Table } from "../components/Table";
+
 export function Booking() {
+  const _columnTitles = ["Nome", "CPF", "E-mail", "Telefone"];
+
   return (
     <>
-      <h1>Booking</h1>
+      <Navbar pathActive={"/"} />
+      <main>
+        <Header />
+        <Table columnTitles={_columnTitles} />
+      </main>
     </>
   );
 }
