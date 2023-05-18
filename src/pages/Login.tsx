68e21e57403e43bdb32f4d09fe494ef9a6dc6433
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-05-18 18:27:35 -0300

    fix: alterando layout de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,53 +19,62 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-8">Login</h1>
-      <div className="w-full max-w-md">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              E-mail
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Digite seu e-mail"
-              value={email}
-              onChange={handleEmailChange}
-            />
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-xl">
+        <div className="flex">
+          <div className="w-2/3">
+            <h1 className="text-3xl font-bold mb-8">Login</h1>
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  E-mail
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Digite seu e-mail"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Senha
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Digite sua senha"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+                {/* Adicione validação de senha aqui, se necessário */}
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                  onClick={handleLogin}
+                >
+                  Entrar
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Senha
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="Digite sua senha"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            {/* Adicione validação de senha aqui, se necessário */}
+          <div className="w-1/3 flex items-center justify-center bg-gray-200 p-4">
+            <p className="text-lg text-gray-800">
+              Desfrute de uma estadia extraordinária no nosso hotel!
+            </p>
           </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={handleLogin}
-            >
-              Entrar
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
