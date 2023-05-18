eb722f4f072facf1fbd3866b5a8341555538a734
NathanHermes <hermesnathan8@gmail.com>
2023-05-17 22:00:38 -0300

    :lipstick: Feat: create layout and design of Client page

--- src/pages/Client.tsx
+++ src/pages/Client.tsx
@@ -1,3 +1,14 @@
+import { Header } from "../components/Header";
+import { Navbar } from "../components/Navbar";
+
 export function Client() {
-  return <h1>Client</h1>;
+  return (
+    <>
+      <Navbar pathActive={"/client"} />
+      <main className="flex flex-col gap-10 items-center justify-center w-full ">
+        <Header title="Hóspede" />
+        {/* <Table columnTitles={_columnTitles} /> */}
+      </main>
+    </>
+  );
 }
