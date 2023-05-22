05145c4c23f772ab5a3f54b5d8549c3f0de10d1b
NathanHermes <hermesnathan8@gmail.com>
2023-05-22 20:23:38 -0300

    :recycle: Refactor: rename variable
    Due to change in export method, import variable rename

--- src/api/reservas/reservas.tsx
+++ src/api/reservas/reservas.tsx
@@ -1,27 +1,25 @@
-import API from "../connection.tsx";
-
-import {Reserva} from "./ReservaModel.tsx";
+import { api } from "../connection.tsx";
 
 export async function save(booking: any) {
-    await API.post('/booking/save', {
-        ...booking
-    })
+  await api.post("/booking/save", {
+    ...booking,
+  });
 }
 
-export async function update(id: string ,booking: any) {
-    await API.put(`/booking/update/${id}`, {
-        ...booking
-    })
+export async function update(id: string, booking: any) {
+  await api.put(`/booking/update/${id}`, {
+    ...booking,
+  });
 }
 
 export async function findById(id: string) {
-    await API.get(`/booking/findBy/${id}`)
+  await api.get(`/booking/findBy/${id}`);
 }
 
 export async function deleteById(id: string) {
-    await API.delete(`/booking/delete/${id}`)
+  await api.delete(`/booking/delete/${id}`);
 }
 
 export async function getAll() {
-    return await API.get(`/booking/getAll`)
+  return await api.get(`/booking/getAll`);
 }
