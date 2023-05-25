401e0eab9536b793c63b679e696c327616fe019f
NathanHermes <hermesnathan8@gmail.com>
2023-05-25 17:08:11 -0300

    Style: rename file of model of client

---
+++ src/api/client/ClientModel.tsx
@@ -0,0 +1,8 @@
+export interface ClientModel {
+  id?: string;
+  name: string;
+  surname: string;
+  cpf: string;
+  email: string;
+  phoneNumber: string;
+}
--- src/api/hospedes/HospedeModel.tsx
+++
@@ -1,8 +0,0 @@
-export interface Hospede {
-    id?: string;
-    nome: string;
-    sobrenome: string;
-    cpf: string;
-    email: string;
-    phoneNumber: string;
-}
