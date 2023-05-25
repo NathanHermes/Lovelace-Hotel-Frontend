bff2ab77c5c94a920a4c5c57b36efe0ea38c5cb2
NathanHermes <hermesnathan8@gmail.com>
2023-05-25 16:51:01 -0300

    Feat: create search function and remane variable and functions

--- src/pages/Client.tsx
+++ src/pages/Client.tsx
@@ -1,96 +1,145 @@
 import { useEffect, useState } from "react";
-import { Header } from "../components/Header";
+import { Header, PageNames } from "../components/Header";
 import { Navbar } from "../components/Navbar";
-import { Table } from "../components/Table";
+import { Table, Titles } from "../components/Table";
 
-import { getAll, deleteById, save, update } from "../api/hospedes/hospedes";
+import {
+  deleteById,
+  save,
+  update,
+  findAllClients,
+  findByName,
+} from "../api/client/Client";
 import { useNavigate } from "react-router-dom";
 import { validateAuth } from "../utils/validateAuth";
+import { ClientModel } from "../api/client/ClientModel";
+import toast, { Toaster } from "react-hot-toast";
 
-export function Client() {
-  const [clients, useClients] = useState([]);
+export const Client = () => {
+  const _columnTitles = ["ID", "Nome", "Sobrenome", "CPF", "Email", "Telefone"];
+  const [clients, setClients] = useState(Array<any>);
   const navigate = useNavigate();
 
   useEffect(() => {
     validateAuth(navigate);
+    loadClients();
+  }, [navigate]);
 
-    getAll().then((res) => {
-      const transformedData = res.data.map((item: any) => {
+  const loadClients = () => {
+    findAllClients().then((response) => {
+      const clientsData = response.data.map((client: ClientModel) => {
         return {
-          id: item.id,
-          name: item.name,
-          surname: item.surname,
-          cpf: item.cpf,
-          email: item.email,
-          phone: item.phoneNumber,
+          id: client.id,
+          name: client.name,
+          surname: client.surname,
+          cpf: client.cpf,
+          email: client.email,
+          phoneNumber: client.phoneNumber,
         };
       });
 
-      useClients(transformedData);
+      setClients(clientsData);
     });
-  }, []);
+  };
+
+  const search = async (fullname: string, researched: boolean) => {
+    if (researched) return loadClients();
+
+    if (fullname.length === 0 || fullname === " ") {
+      return toast("Nome inválido.\nTente novamente.", {
+        icon: "⚠️",
+      });
+    }
+
+    const indexOfLastWhiteSpace = fullname.lastIndexOf(" ");
+    if (indexOfLastWhiteSpace < 0) {
+      return toast("Sobrenome inválido.\nTente novamente.", {
+        icon: "⚠️",
+      });
+    }
+
+    const name = fullname.substring(0, indexOfLastWhiteSpace);
+    const surname = fullname.substring(indexOfLastWhiteSpace + 1);
+    if (surname.length === 0 || surname === " ") {
+      return toast("Sobrenome inválido.\nTente novamente.", {
+        icon: "⚠️",
+      });
+    }
+
+    await findByName(name, surname)
+      .then((response: any) => {
+        setClients([response.data]);
+      })
+      .catch((error: any) => {
+        loadClients();
+
+        if (error.response.status === 400) {
+          toast.error("Hóspede não encontrado.\nTente novamente.");
+        } else {
+          console.log(error);
+        }
+      });
+  };
+
+  const createClient = (client: ClientModel) => {
+    save(client)
+      .then(() => {
+        toast.success("Hóspede cadastrado.");
+        loadClients();
+      })
+      .catch(() => {
+        toast.error(
+          "Não foi possivel cadastrar o hóspede.\nTente novamente mais tarde."
+        );
+      });
+  };
+
+  const updateClient = (client: ClientModel) => {
+    update(client)
+      .then(() => {
+        toast.success("Hóspede atualizado.");
+        loadClients();
+      })
+      .catch(() => {
+        toast.error(
+          "Não foi possivel atualizar o hóspede.\nTente novamente mais tarde."
+        );
+      });
+  };
 
   const deleteClient = (id: string) => {
     deleteById(id)
-      .then((res) => {
-        window.location.reload();
+      .then(() => {
+        toast.success("Hóspede apagado.");
+        loadClients();
       })
-      .catch((err) => {
-        alert(err.response.data);
+      .catch(() => {
+        toast.error(
+          "Não foi possivel apagar o hóspede.\nTente novamente mais tarde."
+        );
       });
   };
-
-  const addClient = (newValue: any) => {
-    let data = {
-      name: newValue.Nome,
-      surname: newValue.Sobrenome,
-      cpf: newValue.CPF,
-      email: newValue.Email,
-      phoneNumber: newValue.Telefone,
-    };
-
-    save(data)
-      .then((res: any) => {
-        window.location.reload();
-      })
-      .catch((err: any) => {
-        alert(err.response.data);
-      });
-  };
-
-  const editClient = (newValue: any) => {      
-    let data = {
-      name: newValue.Nome,
-      surname: newValue.Sobrenome,
-      cpf: newValue.CPF,
-      email: newValue.Email,
-      phoneNumber: newValue.Telefone,
-    };
-
-    update(newValue.id, data)
-    .then((res) => {
-      window.location.reload();
-    })
-    .catch((err) => {
-      alert(err.response.data);
-    });
-  };
-
-  const _columnTitles = ["ID", "Nome", "Sobrenome", "CPF", "Email", "Telefone"];
 
   return (
     <>
       <Navbar pathActive={"/client"} />
       <main className="flex flex-col gap-10 items-center justify-center w-full ">
-        <Header title="Hóspede" inputs={_columnTitles} handleAdd={addClient} />
+        <Header
+          title={PageNames.CLIENT}
+          modalAction={createClient}
+          searchFunction={search}
+        />
+
         <Table
-          title="Hóspede"
+          title={Titles.CLIENT}
           columnTitles={_columnTitles}
           data={clients}
+          editFunction={updateClient}
           deleteFunction={deleteClient}
-          editFunction={editClient}
         />
       </main>
+
+      <Toaster position="top-right" />
     </>
   );
-}
+};
