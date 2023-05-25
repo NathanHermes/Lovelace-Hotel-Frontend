7ef452672f4702ce265fdc9b83c2de94fa12461a
NathanHermes <hermesnathan8@gmail.com>
2023-05-25 17:01:52 -0300

    Style: rename function that find all rooms

--- src/api/room/Room.tsx
+++ src/api/room/Room.tsx
@@ -1,7 +1,7 @@
-import { api } from "../connection.tsx";
 import { RoomModel } from "./RoomModel.tsx";
+import { api } from "../connection.tsx";
 
-export async function findAll() {
+export async function findAllRooms() {
   return await api.get("/room/getAll");
 }
 
